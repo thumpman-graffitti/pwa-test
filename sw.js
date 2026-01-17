9ffa8f68fe56ca1e505607be01d3c2b6f5a7ad6c
thumpman-graffitti <thumpman_graffitti@yahoo.co.jp>
2026-01-17 11:39:26 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,14 @@
+// sw.js（開発用：キャッシュしない）
+
 self.addEventListener("install", event => {
-  console.log("Service Worker installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  clients.claim();
+});
+
+// fetchでは何もしない＝常にネットワークから取得
+self.addEventListener("fetch", event => {
+  // 何もしない
 });
